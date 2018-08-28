06b7579b87bd82783d92fd468eee463004c4c5fc
dwSun <tonghuren@icloud.com>
2018-08-28 17:32:15 +0800

    fix image path

--- classify_image.sh
+++ classify_image.sh
@@ -1,4 +1,4 @@
 python3 classify_image.py \
   --model_file freezed.pb \
   --label_file freezed.label \
-  --image_file ../train/白/0a2afd0597d8e9c7e635012241bbc9eea6622c89.jpg
+  --image_file ../test2/7a74d7bae0f0c8293a866c0320f11154fd2b871c.jpg
